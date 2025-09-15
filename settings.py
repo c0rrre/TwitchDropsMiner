cd91b0a394915dbe86f11c8e786998e7ac70c5c3
c0rrre <joergbuschmann26@gmail.com>
2025-09-15 11:03:31 +0200

    fix apprise

--- settings.py
+++ settings.py
@@ -21,6 +21,7 @@ class SettingsFile(TypedDict):
     connection_quality: int
     tray_notifications: bool
     priority_mode: PriorityMode
+    apprise_url: str
 
 
 default_settings: SettingsFile = {
@@ -33,6 +34,7 @@ default_settings: SettingsFile = {
     "language": DEFAULT_LANG,
     "tray_notifications": True,
     "priority_mode": PriorityMode.PRIORITY_ONLY,
+    "apprise_url": ""
 }
 
 
@@ -55,6 +57,7 @@ class Settings:
     connection_quality: int
     tray_notifications: bool
     priority_mode: PriorityMode
+    apprise_url: str
 
     PASSTHROUGH = ("_settings", "_args", "_altered")
 
